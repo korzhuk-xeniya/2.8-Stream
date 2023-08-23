4b4aeaf9b6a6510aec6a804cef07205d624ecf03
Ксения <korzhuk.xeniya@yandex.ru>
2023-08-23 22:21:04 +0300

    done

--- src/main/java/pro/sky/Employee/exceptions/EmployeeNotFoundException.java
+++ src/main/java/pro/sky/Employee/exceptions/EmployeeNotFoundException.java
@@ -4,5 +4,5 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Сотрудник не найден")
-public class EmployeeNotFoundException extends RuntimeException{
+public class EmployeeNotFoundException extends RuntimeException {
 }
--- src/main/java/pro/sky/Employee/service/DepartmentServiceImpl.java
+++ src/main/java/pro/sky/Employee/service/DepartmentServiceImpl.java
@@ -6,10 +6,12 @@ import pro.sky.Employee.exceptions.EmployeeNotFoundException;
 
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
 @Service
-public class DepartmentServiceImpl implements DepartmentService{
+public class DepartmentServiceImpl implements DepartmentService {
     private EmployeeService employeeService;
 
     public DepartmentServiceImpl(EmployeeService employeeService) {
@@ -35,12 +37,14 @@ public class DepartmentServiceImpl implements DepartmentService{
     @Override
     public Collection<Employee> getEmployeesInDepartment(int department) {
         return employeeService.outputEmployee().stream()
-                .filter(empl -> empl.getDepartment() == department);
+                .filter(empl -> empl.getDepartment() == department)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Map<Integer, Collection<Employee>> getAll() {
+    public Map<Integer, List<Employee>> getAll() {
         return employeeService.outputEmployee().stream()
                 .collect(Collectors.groupingBy(employee -> employee.getDepartment()));
+
     }
 }
