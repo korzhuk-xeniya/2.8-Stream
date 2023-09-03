c170ed6e5d79733cdf9ad2891be6038757a3ccb2
Ксения <korzhuk.xeniya@yandex.ru>
2023-09-03 14:49:00 +0300

    done

--- src/main/java/pro/sky/Employee/exceptions/IlligalSymbolException.java
+++ src/main/java/pro/sky/Employee/exceptions/IlligalSymbolException.java
@@ -3,6 +3,6 @@ package pro.sky.Employee.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Должны быть только буквы")
 public class IlligalSymbolException extends IllegalArgumentException{
 }
--- src/main/java/pro/sky/Employee/util/EmployeeNameValidator.java
+++ src/main/java/pro/sky/Employee/util/EmployeeNameValidator.java
@@ -1,5 +1,14 @@
 package pro.sky.Employee.util;
 
+import org.apache.commons.lang3.StringUtils;
+import pro.sky.Employee.exceptions.IlligalSymbolException;
+
 public class EmployeeNameValidator {
-    public static void  validateIsAlpha(String... names)
+    public static void  validateIsAlpha(String... names) {
+        for (String name : names) {
+             if (!StringUtils.isAlpha(name)) {
+                 throw new IlligalSymbolException();
+             }
+        }
+    }
 }
