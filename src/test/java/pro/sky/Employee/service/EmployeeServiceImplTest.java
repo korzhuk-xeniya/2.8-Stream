8c3b3e842cacf71f28ab719a38c0bb891e4ebb1a
Ксения <korzhuk.xeniya@yandex.ru>
2023-09-30 18:07:16 +0300

    2 commit_mockito

---
+++ src/test/java/pro/sky/Employee/service/EmployeeServiceImplTest.java
@@ -0,0 +1,23 @@
+package pro.sky.Employee.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class EmployeeServiceImplTest {
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void removeEmployee() {
+    }
+
+    @Test
+    void findEmployee() {
+    }
+
+    @Test
+    void outputEmployee() {
+    }
+}
