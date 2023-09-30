715fa6782fa673f988aadf2b4698a8d70f2443ed
Ксения <korzhuk.xeniya@yandex.ru>
2023-09-30 19:07:08 +0300

    new_first

--- src/test/java/pro/sky/Employee/service/EmployeeServiceImplTest.java
+++ src/test/java/pro/sky/Employee/service/EmployeeServiceImplTest.java
@@ -1,12 +1,20 @@
 package pro.sky.Employee.service;
 
 import org.junit.jupiter.api.Test;
+import pro.sky.Employee.dto.Employee;
 
 import static org.junit.jupiter.api.Assertions.*;
 class EmployeeServiceImplTest {
-
+private EmployeeServiceImpl underTest = new EmployeeServiceImpl();
     @Test
-    void addEmployee() {
+    void addEmployee_shouldAddEmployeeToMapAndReturnEmployee() {
+        String firstName = "Alex";
+        String lastName = "Ivanov";
+        int department = 2;
+        double salary = 90_000;
+        Employee employee = new Employee(firstName, lastName,department, salary);
+
+
     }
 
     @Test
